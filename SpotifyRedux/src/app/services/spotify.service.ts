6e520d17228661bad595f20ceca44c77fc7567f6
John Vall <jvall@westmonroepartners.com>
2017-04-17 15:40:27 -0500

    removed commented code

--- SpotifyRedux/src/app/services/spotify.service.ts
+++ SpotifyRedux/src/app/services/spotify.service.ts
@@ -16,19 +16,4 @@ export class SpotifyService {
     return this._http.get(`${this._config.spotifyUrl}/search?q=${artistName}&type=artist`)
       .map(res => res.json().artists.items || []);
   }
-
-  // Well this was a nice idea but not needed
-  // searchArtists(artistName: string): Observable<Artist> {
-  //   return this._http.get(`${this._config.spotifyUrl}/artists?type=artist&q=${artistName}`)
-  //     .mergeMap(response => Observable.from(response.json().items))
-  //     .map(rawArtist => this.mapResponseToArtist(rawArtist));
-  // }
-
-  // private mapResponseToArtist(rawArtistObject: any): Artist {
-  //   return {
-  //     name: rawArtistObject.name,
-  //     imageUrl: rawArtistObject.images[1].url,
-  //     genres: rawArtistObject.genres
-  //   };
-  // }
 }
